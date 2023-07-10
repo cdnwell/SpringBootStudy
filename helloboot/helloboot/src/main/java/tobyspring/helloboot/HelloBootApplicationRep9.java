f17d806be719d6cdb04c0a79b480f556cb6bc4f5
cdnwell <cdnwellhk@gmail.com>
2023-07-10 20:41:23 +0900

    review : study review

--- helloboot/helloboot/src/main/java/tobyspring/helloboot/HelloBootApplicationRep9.java
+++ helloboot/helloboot/src/main/java/tobyspring/helloboot/HelloBootApplicationRep9.java
@@ -19,14 +19,21 @@ public class HelloBootApplicationRep9 {
         serverFactory.setPort(37700);
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
            servletContext.addServlet("frontcontroller", new HttpServlet() {
+               HelloController helloController = new HelloController();
+
                @Override
                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                        String name = req.getParameter("name");
 
+                       String ret = helloController.hello(name);
+
+                       // content type은 굳이 hello 안에서 할 필요는 없을 듯
+                       // servlet에서 특별히 오류를 내지 않으면 200 OK 값을 자동으로 반환해 줌
+                       // 굳이 안해도 되긴 함.
                        resp.setStatus(HttpStatus.OK.value());
                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                       resp.getWriter().println("hello name : " + name);
+                       resp.getWriter().println(ret);
                    } else if (req.getRequestURI().equals("/user")) {
                        resp.setStatus(HttpStatus.OK.value());
                    } else {
