2261ee10c2f2b9c10823a331eab3721d4c16c3ae
cdnwell <cdnwellhk@gmail.com>
2023-08-10 21:35:58 +0900

    study : toby spring boot 2.xx 두개 빈 등록 후 작동 확인

---
+++ helloboot/helloboot/src/main/java/tobyspring/hellobootrep1/HelloController.java
@@ -0,0 +1,25 @@
+package tobyspring.hellobootrep1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@Controller
+public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+}
---
+++ helloboot/helloboot/src/main/java/tobyspring/hellobootrep1/HelloService.java
@@ -0,0 +1,5 @@
+package tobyspring.hellobootrep1;
+
+public interface HelloService {
+    String sayHello(String name);
+}
