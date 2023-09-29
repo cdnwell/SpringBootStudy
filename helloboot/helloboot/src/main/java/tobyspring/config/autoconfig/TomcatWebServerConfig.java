5d0996f9fccb7f9e181f9e1c868e6adcc1cf4b70
cdnwell <cdnwellhk@gmail.com>
2023-09-29 17:40:11 +0900

    study : Environment, System Environment, Property 학습

--- helloboot/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,9 +1,11 @@
 package tobyspring.config.autoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
@@ -13,11 +15,18 @@ import tobyspring.config.MyAutoConfiguration;
 //@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     // 유저 구성 정보, 자동 구성 정보 중 얘보다 먼저 실행된 구성 정보 중 같은 것이 있다면 걔를 등록
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+//        factory.setContextPath(env.getProperty("contextPath"));
+        System.out.println(this.contextPath);
+        factory.setContextPath(this.contextPath);
+        return factory;
     }
 
 //    static class TomcatCondition implements Condition {
--- helloboot/helloboot/src/main/java/tobyspring/hellobootrep1/HellobootApplicationRep5.java
+++ helloboot/helloboot/src/main/java/tobyspring/hellobootrep1/HellobootApplicationRep5.java
@@ -1,6 +1,9 @@
 package tobyspring.hellobootrep1;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
@@ -8,6 +11,14 @@ public class HellobootApplicationRep5 {
     // 2개의 팩토리 메서드를 제거하면 현재 구조에서는 필요한 빈이 제공되지 않으므로 에러 발생
     // 어디선가는 빈 구성정보를 제공해주어야 함.
     // 빼고 싶은데, 이유는 Spring Initializr가 만든 Spring boot 앱과 거의 똑같은 걸 만들고 싶기 때문임.
+
+//    @Bean
+//    ApplicationRunner applicationRunner(Environment env) {
+//        return args -> {
+//            String name = env.getProperty("my.name");
+//            System.out.println("my.name : " + name);
+//        };
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplicationRep5.class, args);
